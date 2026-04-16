213d18998d20a971ba693c7f73d5b7d0b81b2851
Martinitokristan <kristan.martinito@urios.edu.ph>
2026-04-16 15:20:40 +0800

    Fix Sileo toast

--- resources/js/components/shared/SileoToaster.jsx
+++ resources/js/components/shared/SileoToaster.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Toaster, sileo } from "sileo";
-import "../../../../node_modules/sileo/dist/styles.css";
+import "sileo/styles.css";
 
 // Expose globally so every component import works and console testing is possible
 if (typeof window !== 'undefined') {
